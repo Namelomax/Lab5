22e2aef4383b59226a1f7e44598669335cf1aa5c
Namelomax <125127585+Namelomax@users.noreply.github.com>
2023-03-22 16:29:57 +0300

    Add files via upload
    Another return

--- ConsoleRead.java
+++ ConsoleRead.java
@@ -6,32 +6,38 @@ import java.util.Arrays;
 
 public class ConsoleRead {
     static Object parametr;
+    Object[] list;
     public Object[] Read(String txt){
-        LinkedTreeMap<String, String> hm = new LinkedTreeMap<>();
+        LinkedTreeMap<String, String> refactor = new LinkedTreeMap<>();
         txt =txt.replace("{", "").replace("}", "");
-        String[] command1 = txt.split(" ");  // Line со сканера
-        String command = command1[0];
-        String[] com = Arrays.copyOfRange(command1, 1, command1.length);
-        if (com.length >= 1 && com[0].contains("=")) {
+        String[] splittedcommand = txt.split(" ");  // Line со сканера
+        String command = splittedcommand[0];
+        String[] args = Arrays.copyOfRange(splittedcommand, 1, splittedcommand.length);
+        if (args.length >= 1 && args[0].contains("=")) {
             Defaults df = new Defaults();
-            for (String param1 : com) {
-                String[] values = param1.split("=");
-                values[1]=values[1].replace(",","");
-                hm.put(values[0], values[1]);
+            for (String param : args) {
+                String[] values = param.split("=");
+                values[1]=values[1].replace("," , "");
+                refactor.put(values[0], values[1]);
             }
-        df.Defaults(hm);
-            parametr = hm;
-        } else if (com.length == 1 && !com[0].contains("=")) {
-            parametr = Double.parseDouble(com[0]);
+        df.Defaults(refactor);
+            System.out.println();
+            int i =0;
+           /* for(Object value:refactor.values()){
+                list[i++]=value;
+            }*/
+            parametr = refactor;
+           list = new Object[]{command,parametr};
+           System.out.println(Arrays.toString(list));
+        } else if (args.length == 1 && !args[0].contains("=")) {
+            parametr = Double.parseDouble(args[0]);
             System.out.println(parametr);
+            list = new Object[]{command,parametr};
         } else  {
             parametr = null;
+            list = new Object[]{command,parametr};
         }
-        Object[] ar = new Object[2];
-        ar[0] = command;
-        ar[1] = parametr;
-        System.out.println(ar[0]+" "+ar[1]);
-        return ar;  // возвращаемая команда с параметром
+        return list;  // возвращаемая команда с параметром
     }
 
 }
