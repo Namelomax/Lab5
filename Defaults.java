a487c8d4e37bad750abf24cd5a526c23976dd7e7
Namelomax <125127585+Namelomax@users.noreply.github.com>
2023-03-23 23:20:47 +0300

    Add files via upload
    if replaced with algorithm

--- Defaults.java
+++ Defaults.java
@@ -1,59 +1,45 @@
 package Second_sem.lab5;
 
+import Second_sem.lab5.BaseClasses.Car;
+import Second_sem.lab5.BaseClasses.Mood;
 import com.google.gson.internal.LinkedTreeMap;
-
-import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
-
+import java.util.HashMap;
+import java.util.Map;
 public class Defaults {
+    HashMap<String,Object> example = new HashMap<>();
+    ArrayList<Number> ar_coord = new ArrayList<>();
+    int[] ar_date = new int[6];
     public LinkedTreeMap Defaults(LinkedTreeMap txt){
-        LinkedTreeMap<String, Object> b = Main.listOfData.get(0);
-        if (!txt.containsKey("id")) {
-            String line1 = WorkWithFile.readFromFile("id.txt");
-            double a = Double.parseDouble(line1);
-            System.out.println(a);
-            txt.put("id",a);
-            File file = new File("id.txt");
+        String id = WorkWithFile.readFromFile("id.txt");
+        int a = Integer.parseInt(id);
+        txt.put("id",a);
+        ar_coord.add(0); ar_coord.add(0);
+        ar_date[0]= Main.dateOfInitialization.getYear();
+        ar_date[1]= Main.dateOfInitialization.getMonthValue();
+        ar_date[2]= Main.dateOfInitialization.getDayOfMonth();
+        ar_date[3]= Main.dateOfInitialization.getHour();
+        ar_date[4]= Main.dateOfInitialization.getMinute();
+        ar_date[5]= Main.dateOfInitialization.getSecond();
+        example.put("name","Bob");
+        example.put("coordinates",ar_coord);
+        example.put("creationDate", ar_date);
+        example.put("hasToothpick",false);
+        example.put("realHero",false);
+        example.put("impactSpeed",100L);
+        example.put("soundtrackName","Gan");
+        example.put("minutesOfWaiting",5.0);
+        example.put("mood", Mood.LONGING);
+        example.put("car", new Car("GDV"));
+        for (Map.Entry<String, Object> entry : example.entrySet()) {
+            if (txt.get(entry.getKey()) == null || !txt.get(entry.getKey()).equals(txt.get(entry.getKey()))){
+                txt.put(entry.getKey(),entry.getValue());}          }
+
                 try (FileWriter fileWriter = new FileWriter("id.txt")) {
                     fileWriter.write(Double.toString(a+1));
             } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-        }
-        if (!txt.containsKey("name")) {
-            txt.put("name","Bob");
-        }
-        if (!txt.containsKey("coordinates")) {
-            ArrayList<Number> ar_coord = new ArrayList<>(); ar_coord.add(0); ar_coord.add(0);
-            txt.put("coordinates",ar_coord);
-        }
-        if (!txt.containsKey("creationDate")) {
-            ArrayList<Number> ar_date = new ArrayList<>();ar_date.add(2020.0);ar_date.add(3.0); ar_date.add(8.0); ar_date.add(23.0); ar_date.add(48.0); ar_date.add(30.0);
-            txt.put("coordinates",ar_date);
-        }
-        if (!txt.containsKey("realHero")) {
-            txt.put("realHero",false);
-        }
-        if (!txt.containsKey("hasToothpick")) {
-            txt.put("hasToothpick",false);
-        }
-        if (!txt.containsKey("impactSpeed")) {
-            txt.put("impactSpeed",Long.toString(100));
-        }
-        if (!txt.containsKey("soundtrackName")) {
-            txt.put("soundtrackName","Gan");
-        }
-        if (!txt.containsKey("minutesOfWaiting")) {
-            txt.put("minutesOfWaiting",(Double.toString(5.0)));
-        }
-        if (!txt.containsKey("mood")) {
-            txt.put("mood","LONGING");
-        }
-        if (!txt.containsKey("car")) {
-            txt.put("car","GDV");
-        }
-        return txt;
-    }
-}
+        return txt;}}
